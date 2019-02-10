eccf914110e284c0d27b78e723b0d42881e5e876
Jonathan <jmc@devj.uk>
2019-02-10 20:05:12 +0000

    Project Started
    Started the project as a sideline game. Hope to make it on my sunday / casual days where I don't do much and tend to be more chill with the amount of work I do.

---
+++ Project/Assets/Scripts/CanvasController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasController : MonoBehaviour
+{
+	public RectTransform Menu;
+	public RectTransform RedLevels;
+
+	public Vector3 LastPos;
+	public Vector3 CurrentPos;
+
+
+	private void Start()
+	{
+		CurrentPos = Menu.transform.position;
+	}
+
+
+	private void Update()
+	{
+		ChangeContent();
+	}
+
+
+	public void ChangeContent()
+	{
+		
+	}
+}
